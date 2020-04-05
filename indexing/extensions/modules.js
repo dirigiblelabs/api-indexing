14149a6f0d74370b6827a83a0ee732126b0e1094
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:27:40 +0300

    Adds "api-modules" Extension

---
+++ indexing/extensions/modules.js
@@ -0,0 +1,9 @@
+exports.getContent = function() {
+	return [{
+		name: "indexing/v4/searcher",
+		description: "Indexing Searcher API"
+	}, {
+		name: "indexing/v4/writer",
+		description: "Indexing Writer API"
+	}];
+};
